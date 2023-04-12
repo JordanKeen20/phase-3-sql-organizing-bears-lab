b5199b3bc1b09ebff354cc5fef633028e696e176
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2023-04-11 17:01:10 -0700

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 35, "M", "white", "Agressive", 0);
+
+INSERT INTO bears VALUES (2, "Rowdy", 25, "M", "Dark Brown", "Calm", 1);
+
+INSERT INTO bears VALUES (3, "Tabitha", 5, "F", "Black", "Easily Aggivated", 0);
+
+INSERT INTO bears VALUES (4, "Sergeant Brown", 10, "M", "Tan", "Lazy", 0);
+
+INSERT INTO bears VALUES (5, "Melissa", 15, "F", "White", "Anger", 1);
+
+INSERT INTO bears VALUES (6, "Grinch", 4, "M", "Black", "Grouchy", 0);
+
+INSERT INTO bears VALUES (7, "Wendy", 9, "F", "Dark Brown", "Happy", 1);
+
+INSERT INTO bears VALUES (8, NULL, 21, "F", "Tan", "Scared", 1);
